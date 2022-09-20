6ce4574868eab9e0ff0cd2f75d3ed2147369d06d
Charlie <charlie200330@gmail.com>
2022-09-20 10:22:41 +0100

    Got app and container working with the TravelAgent component running inside

---
+++ src/Components/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Container from './Container/Container';
+import TravelAgent from '../TravelAgent/TravelAgent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <TravelAgent />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TravelAgent/TravelAgent.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './TravelAgent.css'
+
+const TravelAgent = () => <div className="TravelAgent"></div>
+
+export default TravelAgent;
